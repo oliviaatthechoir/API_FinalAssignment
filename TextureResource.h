6d298b2e08bfa92d09fd125e7fb4cc3019c28611
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-04-17 13:59:47 +0200

    texture2D wrapper (thus far)

--- TextureResource.h
+++ TextureResource.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "raylib.h"
+#include <string>
+
+class TextureResource {
+public: 
+	TextureResource() : texture{}, loaded(false) {}
+
+	explicit TextureResource(const std::string& path) : texture(LoadTexture(path.c_str())), loaded(true) {}
+
+	~TextureResource() {
+		if (loaded) {
+			UnloadTexture(texture); 
+		}
+	}
+
+
+
+	Texture2D Get() { return texture; }
+	const Texture2D& Get() const { return texture; }
+private: 
+	Texture2D texture; 
+	bool loaded = false; 
+
+};
