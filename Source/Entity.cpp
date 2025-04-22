83862daffb6f8bb6164d621d4685241219579233
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-04-22 16:39:14 +0200

    insta create entity.cpp file

---
+++ Source/Entity.cpp
@@ -0,0 +1,5 @@
+#include "Entity.h"
+
+Entity::Entity(Vector2 pos, Vector2 vel, Vector2 size)
+{
+}
--- Source/Entity.h
+++ Source/Entity.h
@@ -1,2 +1,16 @@
 #pragma once
 #include "raylib.h"
+
+class Entity {
+public: 
+	Vector2 position = { 0,0 }; 
+	Vector2 velocity = { 0, 0 }; 
+	Vector2 size = { 0,0 }; 
+	bool active = true; 
+
+	Entity() = default; 
+
+	Entity(Vector2 pos, Vector2 vel, Vector2 size); 
+
+	void 
+};
