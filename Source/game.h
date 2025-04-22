1b132ea8b42d4f144a165e92d6ad9618fb41fb1b
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-04-22 20:28:38 +0200

    included in game now

--- Source/game.h
+++ Source/game.h
@@ -5,6 +5,7 @@
 #include "TextureResource.h"
 #include "Background.h"
 #include "Player.h"
+#include "Projectile.h"
 #include <string>
 
 //TODO: seperate functionalities here 
@@ -16,33 +17,11 @@ enum struct State
 	ENDSCREEN
 };
 
-enum struct EntityType
-{
-	PLAYER,
-	ENEMY,
-	PLAYER_PROJECTILE,
-	ENEMY_PROJECTILE
-};
 
 
 
-struct Projectile
-{
-public: 
-	// INITIALIZE PROJECTILE WHILE DEFINING IF ITS PLAYER OR ENEMY 
-	Vector2 position = {0,0};
-	int speed = 15; 
-	bool active = true;  
-	EntityType type = {};
 
-	// LINE WILL UPDATE WITH POSITION FOR CALCULATIONS
-	Vector2 lineStart = { 0, 0 };
-	Vector2 lineEnd = { 0, 0 };
 
-	void Update();
-
-	void Render(Texture2D texture) const;
-};
 
 struct Wall 
 {
