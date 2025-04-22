a0f459abe6c1a6de03efb0fb681c1643cb80a297
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-04-23 00:26:57 +0200

    static_casting inside of Projectile::Update();

--- Source/Projectile.cpp
+++ Source/Projectile.cpp
@@ -6,7 +6,7 @@ Projectile::Projectile(Vector2 pos, Vector2 vel) : Entity(pos, vel, {10, 30}) {}
 void Projectile::Update() {
 	Entity::Update(); 
 
-	if (position.y < -size.y || position.y > GetScreenHeight()) {
+	if (position.y < -size.y || position.y > static_cast<float>(GetScreenHeight())) {
 		active = false; 
 	}
 }
