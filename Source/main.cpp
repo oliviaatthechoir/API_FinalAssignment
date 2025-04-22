f09f12dd30b92c642d810c67df75047f1bb34e2c
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-04-23 00:16:17 +0200

    crossing off a todo, have removed each unit into its own file - need to check if im not doing 2 step init for the player

--- Source/main.cpp
+++ Source/main.cpp
@@ -26,7 +26,6 @@
 
 //TODO: clean up main.cpp, doesn't need that many variables
 //  Delete redundant code: raylib has collision code (rect rect intersection is all you need), raylib is GetRandom for random numbers, 
-//TODO: move each strut / class into a separate file (physical structure). Just header file is fine
 //TOOD: use RAII for all resources (texture)
 //TODO: simplify all implementations. use range-for, algoritms (erase_if)
 //      reduce nesting, dont use type flags ("is_enemy_bullet") just keep two separate list of projectiles, one for player, one for alien shots)
